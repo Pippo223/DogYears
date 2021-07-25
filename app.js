1f1161297000913e043bb156ff8e6fd6a7e67d36
Philip Owusu Asare <philip.asare@amalitech.org>
2021-07-25 10:13:14 +0000

    Dog years project codecademy

---
+++ app.js
@@ -0,0 +1,19 @@
+const myAge = 2//my age variable
+
+let earlyYears = 2 //early years variable
+
+earlyYears *= 10.5
+
+let laterYears = myAge - 2
+
+laterYears *= 4 //or laterYears = laterYears * 4
+
+console.log(earlyYears)
+
+console.log(laterYears)
+
+myAgeInDogYears = earlyYears + laterYears
+
+const myName = 'phil'.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
